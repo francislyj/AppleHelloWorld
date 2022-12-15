7779d0f7f834b0c92036bf06f3ba470b3fc36929
yijun.liu <francislyj@gmail.com>
2022-12-15 14:56:13 +0800

    init

--- AppleHelloWorld/ContentView.swift
+++ AppleHelloWorld/ContentView.swift
@@ -10,29 +10,80 @@ import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Button {
-            let utterance = AVSpeechUtterance(string: "hello world");
-            utterance.voice = AVSpeechSynthesisVoice(language: "en-GB");
+
+        VStack {
             
-            let synthesizer = AVSpeechSynthesizer();
-            synthesizer.speak(utterance);
-            
-            
-        } label: {
-            Text("Hello, world!")
+            Text("Guess These Movies")
                 .fontWeight(.bold)
-                .font(.system(.title, design: .rounded))
+                .font(.largeTitle)
+            
+            Text("Can you guess the movie from these emojis?")
+            
+            Text("Tap the button to find out the answer")
+            
+            Button {
+                speak(text: "The answer is Back to the Future!")
+            } label: {
+                Text("👦🏻👴🏻🚗⌚️")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.green)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "The answer is Frozen!")
+            } label: {
+                Text("🏰👭🌀❄️☃️")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.yellow)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "The answer is Ocean 11!")
+            } label: {
+                Text("🌊1️⃣1️⃣")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "The answer is Spiderman!")
+            } label: {
+                Text("🕷🏃")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.brown)
+            .cornerRadius(20)
         }
-        .padding()
-        .background(.purple)
-        .foregroundColor(.white)
-        .cornerRadius(20)
         
+    }
+    
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
+
