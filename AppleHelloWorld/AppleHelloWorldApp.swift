a61c1e4efe6d06f28ca39ba77a59347570c6dc74
yijun.liu <francislyj@gmail.com>
2022-12-15 09:24:03 +0800

    Initial Commit

---
+++ AppleHelloWorld/AppleHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleHelloWorldApp.swift
+//  AppleHelloWorld
+//
+//  Created by yijun.liu on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct AppleHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
