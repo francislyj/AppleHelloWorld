dbc6a25fc4fd8f6f1d038c3914e79dd7d4bd9996
yijun.liu <francislyj@gmail.com>
2022-12-15 09:26:44 +0800

    init

--- AppleHelloWorld/ContentView.swift
+++ AppleHelloWorld/ContentView.swift
@@ -1,16 +1,33 @@
 //
 //  ContentView.swift
-//  AppleHelloWorld
+//  HelloWorld
 //
 //  Created by yijun.liu on 2022/12/15.
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button {
+            let utterance = AVSpeechUtterance(string: "hello world");
+            utterance.voice = AVSpeechSynthesisVoice(language: "en-GB");
+            
+            let synthesizer = AVSpeechSynthesizer();
+            synthesizer.speak(utterance);
+            
+            
+        } label: {
+            Text("Hello, world!")
+                .fontWeight(.bold)
+                .font(.system(.title, design: .rounded))
+        }
+        .padding()
+        .background(.purple)
+        .foregroundColor(.white)
+        .cornerRadius(20)
+        
     }
 }
 
