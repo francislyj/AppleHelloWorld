999df0e675d1152b980d07f69e8f20b0ae1543fe
francis.l <francis.l@binance.com>
2022-12-19 11:40:34 +0800

    [feat] refactor search

---
+++ AppleHelloWorld/StackViewDemo.swift
@@ -0,0 +1,101 @@
+//
+//  StackViewDemo.swift
+//  AppleHelloWorld
+//
+//  Created by user on 2022/12/16.
+//
+
+import SwiftUI
+
+struct StackViewDemo: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            VStack {
+                Text("Instance Developer")
+                    .fontWeight(.bold)
+                    .font(.system(size: 40))
+                    .foregroundColor(.indigo)
+                
+                Text("Get help from experts in  15 minutes")
+            }
+            
+            HStack(alignment: .bottom, spacing: 10) {
+                Image("user1")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                Image("user2")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("user3")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding(.horizontal, 20)
+            
+            Text("Need help with coding problems? Register!")
+
+            
+            Spacer()
+            
+            
+            VSignUpButtonGroup()
+        }
+        .padding(.top, 30)
+        
+    }
+}
+
+struct StackViewDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        Group {
+            StackViewDemo()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+            .previewDisplayName("iPhone 12 Pro")
+            
+            StackViewDemo()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+                .previewDisplayName("iPhone 12 Pro")
+                .previewInterfaceOrientation(.landscapeLeft)
+            
+            StackViewDemo()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+                .previewDisplayName("iPhone 12 Pro Max")
+            
+            StackViewDemo()
+                .previewDevice(PreviewDevice(rawValue: "“iPad Air (4th generation)"))
+                .previewDisplayName("iPad Air")
+        }
+    }
+}
+
+struct VSignUpButtonGroup: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("Sign up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
+            Button {
+                
+            } label: {
+                Text("Log in")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
+    }
+}
